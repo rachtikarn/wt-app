375ea92d5c034ef11b682a85fea19d76d5efc50c
rachtikarn <5910110279@psu.ac.th>
2018-09-30 23:49:51 +0700

    Flex Box2

--- components/Forecast.js
+++ components/Forecast.js
@@ -6,17 +6,24 @@ export default class Forecast extends React.Component {
     render() {
     return (
     <View>
-    <Text>{this.props.main}</Text>
-    <Text>{this.props.description}</Text>
-    <Text>{this.props.temp}</Text>
-    <Text>°C</Text>
-  
+    <Text style={styles.display1}>{this.props.main}</Text>
+    <Text style={styles.display}>{this.props.description}</Text>
+    <Text style={styles.display1}>{this.props.temp}°C</Text>
     </View>
     );
     }
    }
    
-    
+   const styles = StyleSheet.create({
+  display: {
+    fontSize: 15,
+    color: '#003300'
+  },
+  display1: {
+    fontSize: 25,
+    color: '#003300'
+  },
+});
    
    
 
--- components/Weather.js
+++ components/Weather.js
@@ -8,29 +8,40 @@ export default class Weather extends React.Component {
     super(props);
     this.state = {
     forecast: {
-    main: '-', description: '-', temp: 0
+    main: 'main', description: 'description', temp: 0
     }
     }
     }
     render() {
     return (
-    <View style={styles.container}>
-        
-            <Text>Zip code is {this.props.zipCode}.</Text>
+    <View >
+        <ImageBackground source={require('../bg.jpeg')} style={styles.backdrop}>
+        <View style={styles.container}>
+            <Text style={styles.display}>Zip code is {this.props.zipCode}.</Text>
             <Forecast {...this.state.forecast} />
-        
-         
+        </View>
+        </ImageBackground>    
     </View>
             );
         }
    }
    const styles = StyleSheet.create({
-    container: { 
-        paddingTop: 25, 
+    container:{
+        paddingTop: 20,
         flexDirection:'column',
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+        backgroundColor:'#999966',
+        opacity: 0.3,
+        height: '40%'
+        
+      },
+      display: {
+        fontSize: 15,
+        color: '#003300'
+      },
+      backdrop: { 
+        width: '100%', 
+        height: '100%'
     },
-    backdrop: { width: '100%', height: '100%'},
-    
    });
